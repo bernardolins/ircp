70a97d5fe4dac93dc637bd60fa4f97f2c77927f4
bernardolins <bernardo.lins@corp.globo.com>
2018-09-23 15:12:10 -0300

    Add test/support to compilation path

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Alchevents.MixProject do
       app: :alchevents,
       version: "0.1.0",
       elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -17,6 +18,9 @@ defmodule Alchevents.MixProject do
       mod: {Alchevents.Application, []}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
