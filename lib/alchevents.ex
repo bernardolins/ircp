08e0b4580f088b65eb04c8c352a6070bdb99d217
bernardolins <bernardo.lins@corp.globo.com>
2018-09-23 10:51:55 -0300

    Initial commit

---
+++ lib/alchevents.ex
@@ -0,0 +1,18 @@
+defmodule Alchevents do
+  @moduledoc """
+  Documentation for Alchevents.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Alchevents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alchevents_test.exs
@@ -0,0 +1,8 @@
+defmodule AlcheventsTest do
+  use ExUnit.Case
+  doctest Alchevents
+
+  test "greets the world" do
+    assert Alchevents.hello() == :world
+  end
+end
