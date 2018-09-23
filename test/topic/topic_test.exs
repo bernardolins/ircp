1c5936328ba40f0c19a8015916e63026c7c41b27
bernardolins <bernardo.lins@corp.globo.com>
2018-09-23 20:38:49 -0300

    Rename topic listener to test consumer for test usage

--- test/topic/topic_test.exs
+++ test/topic/topic_test.exs
@@ -4,7 +4,7 @@ defmodule Alchevents.TopicTest do
   describe "#publish" do
     test "send event to a worker when it subscribe to the topic" do
       {:ok, pid} = Alchevents.Topic.start_link(:channel, :topic)
-      Alchevents.Support.TopicListener.start_link(pid, self())
+      Alchevents.Support.TestConsumer.start_link(pid, self())
 
       Alchevents.Topic.publish(:channel, :topic, :event1)
       :timer.sleep(100)
