862eb18b0c6f9b908bbf704c3bc948bd7da3873a
bernardolins <bernardolins28@gmail.com>
2018-09-30 14:38:29 -0300

    Improve client interface

--- lib/ircp/client.ex
+++ lib/ircp/client.ex
@@ -1,13 +1,19 @@
 defmodule IRCP.Client do
-  def ask(who, message), do: GenStage.call(who, message)
-  def tell(who, message), do: GenStage.cast(who, message)
-  def join(client, channel_name) do
-    case IRCP.Registry.Channel.lookup(channel_name) do
-      {:ok, {channel, _}} ->
-        GenStage.async_subscribe(client, to: channel)
-        :ok
-      {:error, :not_found} ->
-        {:error, :not_found}
+  def private_question(who, message) do
+    GenStage.call(who, message)
+  end
+
+  def private_message(who, message) do
+    GenStage.cast(who, message)
+  end
+
+  def join(who, channel_name) do
+    with {:ok, {channel, _}} <- IRCP.Registry.Channel.lookup(channel_name),
+         {:ok, _} <- GenStage.sync_subscribe(who, to: channel, channel_name: channel_name)
+    do
+      :ok
+    else
+      error -> error
     end
   end
 
@@ -16,7 +22,13 @@ defmodule IRCP.Client do
       use GenStage
       require Logger
 
-      def start_link(options \\ []) do
+      def set_info(options), do: {:ok, options}
+      def handle_join(_, info), do: {:ok, info}
+      def handle_message(_, state), do: {:noreply, state}
+      def handle_question(_, _, state), do: {:reply, :not_implemented, state}
+      defoverridable [set_info: 1, handle_join: 2, handle_message: 2, handle_question: 3]
+
+      def create(options \\ []) do
         GenStage.start_link(__MODULE__, options)
       end
 
@@ -25,27 +37,24 @@ defmodule IRCP.Client do
         |> Keyword.get(:join_channels, [])
         |> List.wrap
 
-        channel_list = get_channel_list(channel_names)
-
-        with {:ok, info} <- set_info(options),
-             {:ok, info} <- join_list(channel_names, info)
-        do
-          {:consumer, info, subscribe_to: channel_list}
-        else
-          unknown -> {:stop, :bad_return_value, unknown}
+        case set_info(options) do
+          {:ok, info} ->
+            {:consumer, info, subscribe_to: channel_names_to_pid(channel_names)}
+          {:stop, _} = stop ->
+            stop
+          invalid ->
+            {:stop, :bad_return_value}
         end
-      end
-
-      def handle_events(messages, _, state) do
-        handle_messages(messages, state)
       end
 
       def handle_call(message, from, state) do
         case proteced_handle_question(message, from, state) do
-          {:reply, reply, state} ->
-            {:reply, reply, [], state}
-          error ->
-            {:stop, :bad_return_value, error}
+          {:reply, reply, new_state} ->
+            {:reply, reply, [], new_state}
+          {:stop, _} = stop ->
+            stop
+          invalid_return ->
+            {:stop, {:shutdown, :bad_return_value}, state}
         end
       end
 
@@ -53,25 +62,46 @@ defmodule IRCP.Client do
         case proteced_handle_message(message, state) do
           {:noreply, state} ->
             {:noreply, [], state}
-          error ->
-            {:stop, :bad_return_value, error}
+          {:stop, _} = stop ->
+            stop
+          _ ->
+            {:stop, {:shutdown, :bad_return_value}, state}
         end
       end
 
       def handle_info(message, state) do
         case proteced_handle_message(message, state) do
-          {:noreply, state} ->
-            {:noreply, [], state}
-          error ->
-            {:stop, :bad_return_value, error}
+          {:reply, reply, state} ->
+            {:reply, reply, [], state}
+          {:stop, _} = stop ->
+            stop
+          invalid_return ->
+            {:stop, {:bad_return_value, invalid_return}}
         end
       end
 
-      defp get_channel_list(channel_names) do
+      def handle_subscribe(:producer, opts, _, info) do
+        channel_name = opts[:channel_name]
+        case handle_join(channel_name, info) do
+          {:ok, info} ->
+            {:automatic, info}
+          {:stop, _} = stop ->
+            stop
+          invalid_return ->
+            {:stop, {:shutdown, :bad_return_value}, info}
+        end
+      end
+
+      def handle_events(messages, _, state) do
+        handle_messages(messages, state)
+      end
+
+      defp channel_names_to_pid([]), do: []
+      defp channel_names_to_pid(channel_names) do
         Enum.reduce(channel_names, [], fn(channel_name, list) ->
           case IRCP.Registry.Channel.lookup(channel_name) do
             {:ok, {pid, _}} ->
-              [{pid, channel: channel_name}|list]
+              [{pid, channel_name: channel_name}|list]
             {:error, :not_found} ->
               Logger.warn("Channel not found: #{inspect channel_name}")
               list
@@ -79,30 +109,13 @@ defmodule IRCP.Client do
         end)
       end
 
-      def handle_subscribe(:producer, opts, _, info) do
-        channel_name = opts[:channel_name]
-        case handle_join(channel_name, info) do
-          {:ok, info} -> join_list(channel_list, info)
-          error -> {:stop, :bad_return_value, error}
-        end
-        {:automatic, info}
-      end
-
-      defp join_list([], info), do: {:ok, info}
-      defp join_list([channel_name|channel_list], info) do
-        case handle_join(channel_name, info) do
-          {:ok, info} -> join_list(channel_list, info)
-          error -> {:stop, :bad_return_value, error}
-        end
-      end
-
       defp handle_messages([], state), do: {:noreply, [], state}
       defp handle_messages([message|messages], state) do
         case proteced_handle_message(message, state) do
           {:noreply, new_state} ->
             handle_messages(messages, new_state)
-          error ->
-            {:stop, :bad_return_value, error}
+          _ ->
+            {:stop, :normal, nil}
         end
       end
 
@@ -110,7 +123,10 @@ defmodule IRCP.Client do
         try do
           handle_message(message, state)
         rescue
-          FunctionClauseError -> {:noreply, state}
+          FunctionClauseError ->
+            {:noreply, state}
+          CaseClauseError ->
+            {:noreply, state}
         end
       end
 
@@ -118,15 +134,21 @@ defmodule IRCP.Client do
         try do
           handle_question(message, from, state)
         rescue
-          FunctionClauseError -> {:reply, :not_implemented, state}
+          FunctionClauseError ->
+            {:reply, :not_implemented, state}
+          CaseClauseError ->
+            {:reply, :not_implemented, state}
         end
       end
 
-      def set_info(options), do: {:ok, options}
-      def handle_join(_, info), do: {:ok, info}
-      def handle_message(_, state), do: {:noreply, state}
-      def handle_question(_, _, state), do: {:reply, :not_implemented, state}
-      defoverridable [set_info: 1, handle_join: 2, handle_message: 2, handle_question: 3]
+      defp assert_async_response({:noreply, new_state}), do: {:noreply, [], new_state}
+      defp assert_async_response(invalid_return), do: call_stop(:bad_return_value, invalid_return, nil)
+
+      defp assert_sync_response({:reply, reply, new_state}), do: {:reply, reply, [], new_state}
+      defp assert_sync_response(invalid_return), do: call_stop(:bad_return_value, invalid_return, nil)
+
+      defp call_stop(reason, message, new_state), do: {:stop, {reason, message}, new_state}
+      defp call_stop(reason, message), do: {:stop, {reason, message}}
     end
   end
 end
--- test/support/clients/test_client.ex
+++ test/support/clients/test_client.ex
@@ -1,24 +1,50 @@
-defmodule IRCP.Support.TestClient do
+defmodule IRCP.Support.ValidationClient do
   use IRCP.Client
 
-  def join(_, opts) do
-    test_pid = Keyword.get(opts, :pid, nil)
-    message = Keyword.get(opts, :message, "")
-    state = Keyword.get(opts, :state, 0)
-    if test_pid, do: send(test_pid, {:message, message})
-    {:ok, state}
+  def set_info(options) do
+    case options do
+      [invalid_return: true] ->
+        {:invalid, :invalid_return}
+      [pid: test_process_id] ->
+        send(test_process_id, {:set_info_called, self() })
+        {:ok, test_process_id}
+      [pid: test_process_id, join_channels: _] ->
+        {:ok, test_process_id}
+      _ ->
+        {:ok, nil}
+    end
   end
 
-  def handle_message({:send_to, pid, message}, state) do
-    send(pid, {:message, message})
-    {:noreply, state}
+  def handle_join(channel, test_process_id) do
+    case channel do
+      :invalid_return ->
+        {:invalid, :invalid_return}
+      :test_callback ->
+        send(test_process_id, {:handle_join_called, channel, self()})
+        {:ok, test_process_id}
+      _ ->
+        {:ok, test_process_id}
+    end
   end
 
-  def handle_question({:return_message, message}, _from, state) do
-    {:reply, message, state}
+  def handle_message(message, test_process_id) do
+    case message do
+      :invalid_return ->
+        {:invalid, :invalid_return}
+      :test_callback ->
+        send(test_process_id, {:handle_message_called, self()})
+        {:noreply, test_process_id}
+      _ ->
+        {:noreply, test_process_id}
+    end
   end
 
-  def handle_question(:return_state, _from, state) do
-    {:reply, state, state}
+  def handle_question(message, _, test_process_id) do
+    case message do
+      :invalid_return ->
+        {:invalid, :invalid_response, :invalid_return}
+      :test_callback ->
+        {:reply, :handle_question_called, test_process_id}
+    end
   end
 end
