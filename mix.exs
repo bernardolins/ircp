67cffa6d9450ccb296342f7b9d362ce5348b1de7
bernardolins <bernardolins28@gmail.com>
2018-10-03 22:07:16 -0300

    Add support to elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule IRCP.MixProject do
     [
       app: :ircp,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
